5cf105a3c225e1d5bbc24ef9413762312f94dbd5
amandachungg <amandachung68@gmail.com>
2023-10-20 12:53:23 +0100

    commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!"); //This is the window.alert() method
+
+var Sent1 = "This is the beginning of the string ";
+var Sent2 = "and this is the end of the string. "
+document.write(Sent1 + Sent2); // Concatenating the variables together and using the document.write() method
+
+document.write(6-2); //This is an expression
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\" <br> \"Eat my shorts!\" Bart responded.");
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var Family = "The Murphys", Dad = "Henry", Mom = "Sarah", Daughter = "Helen", Son = "Jake";
+document.write(Son);
+
+
