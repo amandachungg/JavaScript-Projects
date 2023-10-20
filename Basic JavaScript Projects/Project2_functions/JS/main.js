a9815f96e85f7518af5e225263c5419f2bdef925
amandachungg <amandachung68@gmail.com>
2023-10-20 13:17:28 +0100

    commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function(){
+    var str = Date()
+    document.getElementById("Button_Date").innerHTML = str;
+
+}
+
+function My_Second_Function(){
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
