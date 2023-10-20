45387527d3ec358f49856fb0872ff42123c56aa3
amandachungg <amandachung68@gmail.com>
2023-10-20 13:22:57 +0100

    commit

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,9 +1,12 @@
+// Utilizing the document.getElementById method
 function My_First_Function(){
     var str = Date()
     document.getElementById("Button_Date").innerHTML = str;
 
 }
 
+
+// Utilizing the += operator to concatenate a string
 function My_Second_Function(){
     var sentence = "I am learning";
     sentence += " a lot from this book!";
